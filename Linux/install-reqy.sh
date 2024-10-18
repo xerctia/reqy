03d7923025e040ac4cbc3a814750927e2a43e9d6
Amartya Chowdhury <work.amar05@yahoo.com>
2024-10-18 20:38:27 +0530

    fix Linux installation

--- Linux/install-reqy.sh
+++ Linux/install-reqy.sh
@@ -4,7 +4,7 @@
 sudo mkdir -p /usr/local/share/reqy
 
 # Download the JAR file
-wget https://github.com/xerctia/reqy/releases/download/v1.0/reqy.jar -O /usr/local/share/reqy/reqy.jar
+sudo wget https://github.com/xerctia/reqy/releases/download/v1.0/reqy.jar -O /usr/local/share/reqy/reqy.jar
 
 # Create and download the `reqy` wrapper script
 echo '#!/bin/bash' | sudo tee /usr/local/bin/reqy > /dev/null
